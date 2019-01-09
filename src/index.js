8a1c1fbc2dfff247df0354da8cbe158982ea6d78
Tim Shenin <t.shenin@adguard.com>
2019-01-09 13:03:42 +0300

    Add jsdoc for data object

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import { resolveDependencies } from './injector';
-
 import * as scriptlets from './scriptlets';
 
+/**
+ * Global scriptlet variable
+ */
 scriptlet = (() => {
 
+    /**
+     * Public method to run scriptlet execution
+     * 
+     * @param {Object} data params object
+     * @property {string}  data.name Scriptlets name
+     * @property {'extension'|'corelibs'}  data.engine Platform where scriptlet will be executed
+     * @property {string}  data.version Engine version
+     * @property {Function}  data.hit This function needs to be called when scriptlet was executed and done its work
+     * @property {Array<string>}  data.args Arguments which need to pass in scriptlet
+     */
     const invoke = (data) => {
         if (!data.name) {
             return;
