de6dcd56c9052ad59c3aa2d536fd67cba75bb50e
Maxim Topchu <maximtop@gmail.com>
2019-03-21 14:39:27 +0300

    feature/6 fix eslint warning

--- src/scriptlets/prevent-addEventListener.js
+++ src/scriptlets/prevent-addEventListener.js
@@ -5,8 +5,8 @@ import { toRegExp } from '../helpers/string-utils';
  * Prevents adding event listeners
  *
  * @param {Source} source
- * @param {string} [event] - event name or regexp matching event name
- * @param {string} [funcStr] - string or regexp matching stringified handler function
+ * @param {string|RegExp} [event] - event name or regexp matching event name
+ * @param {string|RegExp} [funcStr] - string or regexp matching stringified handler function
  */
 export function preventAddEventListener(source, event, funcStr) {
     const hit = source.hit
@@ -17,13 +17,15 @@ export function preventAddEventListener(source, event, funcStr) {
     funcStr = funcStr ? toRegExp(funcStr) : toRegExp('/.?/');
 
     const nativeAddEventListener = window.EventTarget.prototype.addEventListener;
-    window.EventTarget.prototype.addEventListener = function (eventName, callback, ...args) {
+    function addEventListenerWrapper(eventName, callback, ...args) {
         if (event.test(eventName.toString()) && funcStr.test(callback.toString())) {
             hit();
             return undefined;
         }
         return nativeAddEventListener.apply(this, [eventName, callback, ...args]);
-    };
+    }
+
+    window.EventTarget.prototype.addEventListener = addEventListenerWrapper;
 }
 
 preventAddEventListener.names = [
