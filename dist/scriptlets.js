5d7250d2970d6ce31b3aa0b098db3f7395903c12
Maxim Topchu <maximtop@gmail.com>
2019-04-12 11:19:07 +0300

    update comment message

--- dist/scriptlets.js
+++ dist/scriptlets.js
@@ -934,33 +934,31 @@
       return funcString.match(regexp)[1];
     };
     /**
-     Example of how works wrapInIIFE
-
-     const source = {
-            args: ["aaa", "bbb"],
-            name: "noeval",
-        };
-
-     const code = "function noeval(source, args) {
-                        alert(source);
-                      }
-     noeval.apply(this, args);"
-
-     const result = wrapInIIFE(source, code);
-
-     result becomes a string
-
-     "(function(source, args){
-            function noeval(source) {
-                alert(source);
-            }
-            noeval.apply(this, args);
-        )({"args": ["aaa", "bbb"], "name":"noeval"}, ["aaa", "bbb"])
-
-     */
-
-    /**
      * Wrap function into IIFE (Immediately invoked function expression)
+     *
+     * <code>
+     *       const source = {
+     *           args: ["aaa", "bbb"],
+     *           name: "noeval",
+     *       };
+     *
+     *      const code = "function noeval(source, args) {
+     *                            alert(source);
+     *                          }
+     *      noeval.apply(this, args);"
+     *
+     *      const result = wrapInIIFE(source, code);
+     *
+     *      // result becomes a string
+     *
+     *      "(function(source, args){
+     *                function noeval(source) {
+     *                    alert(source);
+     *                }
+     *                noeval.apply(this, args);
+     *        )({"args": ["aaa", "bbb"], "name":"noeval"}, ["aaa", "bbb"])"
+     * </code>
+     *
      * @param {Source} source - object with scriptlet properties
      * @param {string} code - scriptlet source code with dependencies
      * @return {string} full scriptlet code
