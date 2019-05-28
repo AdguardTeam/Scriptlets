07fabbbfc752e261f70309fbf050350572aff282
Maxim Topchu <maximtop@gmail.com>
2019-05-28 13:49:12 +0300

    update version and add description

--- dist/scriptlets.js
+++ dist/scriptlets.js
@@ -1321,7 +1321,8 @@
      * @property {Array<string>} args Arguments for scriptlet function
      * @property {'extension'|'corelibs'} engine Defines the final form of scriptlet string presentation
      * @property {string} [version]
-     * @property {string} [hit] Will be executed when target action is triggered
+     * @property {string} [hit] Function string representation from the external environment, which will
+     * be executed when target action is triggered. It can be used to print debug information.
      * @property {string} [ruleText] Source rule text is used for debugging purposes
      */
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { getScriptletCode } from './injector';
  * @property {Array<string>} args Arguments for scriptlet function
  * @property {'extension'|'corelibs'} engine Defines the final form of scriptlet string presentation
  * @property {string} [version]
- * @property {string} [hit] Will be executed when target action is triggered
+ * @property {string} [hit] Function string representation from the external environment, which will
+ * be executed when target action is triggered. It can be used to print debug information.
  * @property {string} [ruleText] Source rule text is used for debugging purposes
  */
 
