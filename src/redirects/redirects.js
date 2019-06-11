5cc5cdb58146427c997a50d9e60dac80eef6e418
Maxim Topchu <maximtop@gmail.com>
2019-06-11 18:38:20 +0300

    feature/redirects expose methods to return all source data

--- src/redirects/redirects.js
+++ src/redirects/redirects.js
@@ -1,9 +1,28 @@
 import jsYaml from 'js-yaml';
 
+/**
+ * RedirectSource
+ * e.g.
+ * {
+ *      title: 1x1-transparent.gif
+ *      comment: http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
+ *      contentType: image/gif;base64
+ *      content: R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+ * }
+ * @typedef {Object} RedirectSource
+ * @property {string} title
+ * @property {string} comment
+ * @property {string} content
+ * @property {string} contentType
+ */
+
+
 class Redirects {
     /**
      * Converts rawYaml into JS object with sources titles used as keys
      * @param rawYaml
+     * @returns {Object<RedirectSource>} - return object with titles in the keys and RedirectSources
+     * in the values
      */
     constructor(rawYaml) {
         try {
@@ -22,12 +41,39 @@ class Redirects {
     }
 
     /**
-     * Returns string representation of source by title
+     * Returns redirect source object
      * @param {string} title
-     * @return {string}
+     * @return {RedirectSource}
      */
     getSource(title) {
+        return this.redirects[title];
+    }
+
+    /**
+     * Returns content of source object by title
+     * @param {string} title
+     * @returns {string}
+     */
+    getContent(title) {
         return this.redirects[title].content;
+    }
+
+    /**
+     * Returns contentType of source object by title
+     * @param {string} title
+     * @returns {string}
+     */
+    getContentType(title) {
+        return this.redirects[title].contentType;
+    }
+
+    /**
+     * Returns comment of source object by title
+     * @param {string} title
+     * @returns {string}
+     */
+    getCommentType(title) {
+        return this.redirects[title].comment;
     }
 }
 
