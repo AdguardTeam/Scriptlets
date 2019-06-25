effa5df733da6d80257b4cc9af3962ee16259964
Tim Shenin <t.shenin@adguard.com>
2019-06-25 17:28:17 +0300

    Add .env file

--- browserstack.js
+++ browserstack.js
@@ -2,6 +2,11 @@
 const browserstackRunner = require('browserstack-runner');
 const config = require('./browserstack.json');
 
+if (!process.env.TRAVIS) {
+    // eslint-disable-next-line global-require
+    require('dotenv').config();
+}
+
 config.username = process.env.BROWSERSTACK_USER;
 config.key = process.env.BROWSERSTACK_KEY;
 
