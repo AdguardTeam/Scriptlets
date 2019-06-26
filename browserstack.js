d4834a962d100e85a2739b3eb2a3f92eec38ac69
Tim Shenin <t.shenin@adguard.com>
2019-06-26 19:27:42 +0300

    throw an error if tests failed

--- browserstack.js
+++ browserstack.js
@@ -10,10 +10,19 @@ if (!process.env.TRAVIS) {
 config.username = process.env.BROWSERSTACK_USER;
 config.key = process.env.BROWSERSTACK_KEY;
 
-browserstackRunner.run(config, (error) => {
+browserstackRunner.run(config, (error, report) => {
     if (error) {
         console.log(`Error: ${error}`);
         return;
     }
+
+    const allTestsPassed = report
+        .map(obj => obj.tests)
+        .every(test => test.status === 'passed');
+
+    if (!allTestsPassed) {
+        throw new Error('Not all tests passed');
+    }
+
     console.log('Test Finished');
 });
