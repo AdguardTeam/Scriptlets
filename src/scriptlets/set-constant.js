7d70618b0158b0d130a2ee5e7cf52e3e30bb6c7f
Adam <adam@adguard.com>
2019-11-20 19:13:50 +0100

    Fix #55

--- src/scriptlets/set-constant.js
+++ src/scriptlets/set-constant.js
@@ -42,7 +42,9 @@ export function setConstant(source, property, value) {
         if (Math.abs(constantValue) > 0x7FFF) {
             return;
         }
-    } else if (value === '') {
+    } else if (value === '-1') {
+        constantValue = -1;
+    }  else if (value === '') {
         constantValue = '';
     } else {
         return;
