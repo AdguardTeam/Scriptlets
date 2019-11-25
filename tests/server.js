68fd0a16ab9bb5373324e56370cfefb055b9917f
Maxim Topchu <maximtop@gmail.com>
2019-11-25 12:40:26 +0300

    use own static files server implementation

--- tests/server.js
+++ tests/server.js
@@ -1,16 +1,28 @@
 /* eslint-disable no-console */
 const http = require('http');
-const ServeStatic = require('serve-static');
-
-const serve = new ServeStatic('tests/dist', { index: ['tests.html'] });
-const requestHandler = (request, response) => {
-    serve(request, response, () => {});
-};
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 8585;
-const server = http.createServer(requestHandler);
 
-server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+const server = http.createServer((req, res) => {
+    const indexFile = 'tests.html';
+    const filename = req.url === '/' ? indexFile : req.url;
+    fs.readFile(path.join(__dirname, 'dist', filename), (err, data) => {
+        if (err) {
+            console.log(err.message);
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at port: ${PORT}`);
+});
 
 module.exports = {
     server,
