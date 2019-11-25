ebb8670f6e036f176a7f94535e7385e5d0ed94ca
Maxim Topchu <maximtop@gmail.com>
2019-11-25 17:40:20 +0300

    add comments

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ yarn install
 
 yarn test
 
+# we do not run browserstack if environment has $TRAVIS variable
+# or if it is pull request from fork $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG
 if [[ ! $TRAVIS || ($TRAVIS && ( $TRAVIS_PULL_REQUEST_SLUG == "$TRAVIS_REPO_SLUG" || $TRAVIS_PULL_REQUEST_SLUG == "" )) ]];
 then
   yarn browserstack
