94a39c3d29e590fc66a46a48d4f9b1b8566e035b
Slava Leleka <v.leleka@adguard.com>
2020-04-07 14:18:39 +0300

    add raf scriptlet + test file

--- src/scriptlets/prevent-requestAnimationFrame.js
+++ src/scriptlets/prevent-requestAnimationFrame.js
@@ -1,5 +1,5 @@
 import {
-    hit,
+    hit, startsWith, toRegExp, noopFunc,
 } from '../helpers';
 
 /* eslint-disable max-len */
@@ -30,6 +30,40 @@ import {
 
 // eslint-disable-next-line no-unused-vars
 export function preventRequestAnimationFrame(source, match) {
+    const nativeRequestAnimationFrame = window.requestAnimationFrame;
+
+    const log = console.log.bind(console); // eslint-disable-line no-console
+
+    // logs requestAnimationFrame to console if no arguments have been specified
+    const shouldLog = typeof match === 'undefined';
+
+    const INVERT_MARKER = '!';
+
+    const doNotMatch = startsWith(match, INVERT_MARKER);
+    if (doNotMatch) {
+        match = match.slice(1);
+    }
+
+    match = match ? toRegExp(match) : toRegExp('/.?/');
+
+    const rafWrapper = (callback, ...args) => {
+        let shouldPrevent = false;
+        if (shouldLog) {
+            hit(source);
+            log(`requestAnimationFrame("${callback.toString()}")`);
+        } else {
+            shouldPrevent = match.test(callback.toString()) !== doNotMatch;
+        }
+
+        if (shouldPrevent) {
+            hit(source);
+            return nativeRequestAnimationFrame(noopFunc);
+        }
+
+        return nativeRequestAnimationFrame.apply(window, [callback, ...args]);
+    };
+
+    window.requestAnimationFrame = rafWrapper;
 }
 
 preventRequestAnimationFrame.names = [
@@ -40,4 +74,4 @@ preventRequestAnimationFrame.names = [
     'ubo-raf-if.js',
 ];
 
-preventRequestAnimationFrame.injections = [hit];
+preventRequestAnimationFrame.injections = [hit, startsWith, toRegExp, noopFunc];
