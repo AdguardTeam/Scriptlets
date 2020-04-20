195411e7505333c68c587488aad05db21bf1bb3f
Slava Leleka <v.leleka@adguard.com>
2020-04-20 15:18:19 +0300

    update raf aliases

--- dist/cjs/scriptletsCjs.js
+++ dist/cjs/scriptletsCjs.js
@@ -3259,7 +3259,7 @@ function preventRequestAnimationFrame(source, match) {
 
   window.requestAnimationFrame = rafWrapper;
 }
-preventRequestAnimationFrame.names = ['prevent-requestAnimationFrame', 'requestAnimationFrame-if.js', 'ubo-requestAnimationFrame-if.js', 'raf-if.js', 'ubo-raf-if.js'];
+preventRequestAnimationFrame.names = ['prevent-requestAnimationFrame', 'no-requestAnimationFrame-if.js', 'ubo-no-requestAnimationFrame-if.js', 'norafif.js', 'ubo-norafif.js', 'ubo-no-requestAnimationFrame-if', 'ubo-norafif'];
 preventRequestAnimationFrame.injections = [hit, startsWith, toRegExp, noopFunc];
 
 /**
