45247f7c1a320147be55fc0288a8e2fc64828ad3
Slava Leleka <v.leleka@adguard.com>
2020-04-23 15:39:42 +0300

    simplify checking and add comments

--- src/redirects/googlesyndication-adsbygoogle.js
+++ src/redirects/googlesyndication-adsbygoogle.js
@@ -33,21 +33,20 @@ export function GoogleSyndicationAdsByGoogle(source) {
     let executed = false;
     for (let i = 0; i < adElems.length; i += 1) {
         const adElemChildNodes = adElems[i].childNodes;
+        const childNodesQuantity = adElemChildNodes.length;
         // childNodes of .adsbygoogle can be defined if scriptlet was executed before
         // so we should check are that childNodes exactly defined by us
         // TODO: remake after scriptlets context developing in 1.3
         let areIframesDefined = false;
-        if (adElemChildNodes.length > 0) {
-            // Object.values() is not supported in IE 11
-            areIframesDefined = Object
-                .keys(adElemChildNodes)
-                .map((key) => adElemChildNodes[key])
-                .reduce((acc, el) => {
-                    return el.tagName.toLowerCase() === 'iframe'
-                        && (el.id.indexOf(ASWIFT_IFRAME_MARKER) > -1
-                            || el.id.indexOf(GOOGLE_ADS_IFRAME_MARKER) > -1)
-                        && acc;
-                }, true);
+        if (childNodesQuantity > 0) {
+            // it should be only 2 child iframes if scriptlet was executed
+            areIframesDefined = childNodesQuantity === 2
+                // the first of child nodes should be aswift iframe
+                && adElemChildNodes[0].tagName.toLowerCase() === 'iframe'
+                && adElemChildNodes[0].id.indexOf(ASWIFT_IFRAME_MARKER) > -1
+                // the second of child nodes should be google_ads iframe
+                && adElemChildNodes[1].tagName.toLowerCase() === 'iframe'
+                && adElemChildNodes[1].id.indexOf(GOOGLE_ADS_IFRAME_MARKER) > -1;
         }
 
         if (!areIframesDefined) {
