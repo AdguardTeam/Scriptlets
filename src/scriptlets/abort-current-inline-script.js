d710278ae85b898a68c196a0019f2ce47f2b632f
Slava Leleka <v.leleka@adguard.com>
2020-06-10 19:36:46 +0300

    update acis doc

--- src/scriptlets/abort-current-inline-script.js
+++ src/scriptlets/abort-current-inline-script.js
@@ -30,6 +30,10 @@ import {
  * - `property` - required, path to a property (joined with `.` if needed). The property must be attached to `window`
  * - `search` - optional, string or regular expression that must match the inline script contents. If not set, abort all inline scripts which are trying to access the specified property
  *
+ * > Note please that for inline script with addEventListener in it
+ * `property` should be set as `EventTarget.prototype.addEventListener`,
+ * not just `addEventListener`.
+ *
  * **Examples**
  * 1. Aborts all inline scripts trying to access `window.alert`
  *     ```
