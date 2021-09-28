02f0e65ec5bc971db0f7179b5a8561837a6363d6
Stanislav Atroschenko <s.atroschenko@adguard.com>
2021-09-28 17:18:47 +0300

    Pull request #224: AG-10255 fix adsbygoogle redirect bug
    Merge in ADGUARD-FILTERS/scriptlets from fix/AG-10255 to master
    
    Squashed commit of the following:
    
    commit e254da5faf230c96094b666ecd4597d79fbbfc08
    Author: caffeine <s.atroschenko@adguard.com>
    Date:   Tue Sep 28 13:31:07 2021 +0300
    
        fix adsbygoogle redirect bug

--- src/redirects/googlesyndication-adsbygoogle.js
+++ src/redirects/googlesyndication-adsbygoogle.js
@@ -39,17 +39,17 @@ export function GoogleSyndicationAdsByGoogle(source) {
         const adElemChildNodes = adElems[i].childNodes;
         const childNodesQuantity = adElemChildNodes.length;
         // childNodes of .adsbygoogle can be defined if scriptlet was executed before
-        // so we should check are that childNodes exactly defined by us
+        // so we should check that childNodes are exactly defined by us
         // TODO: remake after scriptlets context developing in 1.3
         let areIframesDefined = false;
         if (childNodesQuantity > 0) {
             // it should be only 2 child iframes if scriptlet was executed
             areIframesDefined = childNodesQuantity === 2
                 // the first of child nodes should be aswift iframe
-                && adElemChildNodes[0].tagName.toLowerCase() === 'iframe'
+                && adElemChildNodes[0].nodeName.toLowerCase() === 'iframe'
                 && adElemChildNodes[0].id.indexOf(ASWIFT_IFRAME_MARKER) > -1
                 // the second of child nodes should be google_ads iframe
-                && adElemChildNodes[1].tagName.toLowerCase() === 'iframe'
+                && adElemChildNodes[1].nodeName.toLowerCase() === 'iframe'
                 && adElemChildNodes[1].id.indexOf(GOOGLE_ADS_IFRAME_MARKER) > -1;
         }
 
