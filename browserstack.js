c8b5ea44390565f5e5acb2710b2d351c57c3869e
Slava Leleka <v.leleka@adguard.com>
2022-03-01 22:08:00 +0300

    fix WORKER_TIMEOUT_LIMIT

--- browserstack.js
+++ browserstack.js
@@ -6,7 +6,7 @@ const config = require('./browserstack.json');
 
 const TESTS_DIST = './tests/dist';
 const TEST_FILE_NAME_MARKER = '.html';
-const WORKER_TIMEOUT_LIMIT = 10 * 60; // in seconds, 10 minutes per each browserstack worker
+const WORKER_TIMEOUT_LIMIT = 20 * 60; // in seconds, 20 minutes per each browserstack worker
 
 if (!process.env.TRAVIS) {
     // eslint-disable-next-line global-require
