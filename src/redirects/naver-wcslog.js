5d11329b2cd3f1f81054b8c3c2947736594a53c2
Stanislav Atroschenko <s.atroschenko@adguard.com>
2022-03-15 13:24:31 +0300

    add wcslog redirect #94 AG-3906
    Merge in ADGUARD-FILTERS/scriptlets from fix/AG-3906 to release/v1.6
    
    Squashed commit of the following:
    
    commit 31538d4d68dc618e2058cc570f81464231964804
    Merge: 9787ee7 69a8825
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Tue Mar 15 13:18:52 2022 +0300
    
        Merge branch 'release/v1.6' into fix/AG-3906
    
    commit 9787ee7e510b152e53672c5ef55d33bba2660fb0
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Tue Mar 15 12:50:46 2022 +0300
    
        fix typo
    
    commit 807b9eb27e2def21cf135e9d61a14a8c733ec510
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Tue Mar 15 12:31:54 2022 +0300
    
        rename & add to compatibility table
    
    commit 045a8360d941419de83320cf9e5e59f1068e0ee3
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Mon Mar 14 18:47:36 2022 +0300
    
        add wcslog redirect

---
+++ src/redirects/naver-wcslog.js
@@ -0,0 +1,28 @@
+/* eslint-disable func-names */
+import { hit, noopFunc } from '../helpers';
+
+/**
+ * @redirect naver-wcslog
+ *
+ * @description
+ * Mocks wcslog.js of Naver Analytics.
+ *
+ * **Example**
+ * ```
+ * ||wcs.naver.net/wcslog.js$script,redirect=naver-wcslog
+ * ```
+ */
+
+export function NaverWcslog(source) {
+    window.wcs_add = {};
+    window.wcs_do = noopFunc;
+    window.wcs = {
+        inflow: noopFunc,
+    };
+
+    hit(source);
+}
+
+NaverWcslog.names = ['naver-wcslog'];
+
+NaverWcslog.injections = [hit, noopFunc];
--- src/redirects/redirects-list.js
+++ src/redirects/redirects-list.js
@@ -22,3 +22,4 @@ export * from './tagcommander-tc.js';
 export * from './didomi-loader.js';
 export * from './prebid.js';
 export * from './prebid-ads.js';
+export * from './naver-wcslog.js';
