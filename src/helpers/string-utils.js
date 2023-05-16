2cbabc7b994e2415475e0434209a528e427f147f
Stanislav Atroschenko <s.atroschenko@adguard.com>
2023-05-16 15:50:00 +0300

    Pull request #502: AG-19184 unescape quotes for callback matching argument
    Merge in ADGUARD-FILTERS/scriptlets from fix/AG-19184 to master
    
    Squashed commit of the following:
    
    commit bc0fcf16c82276dbd4841aca712a1b81385851ab
    Merge: 84298820 71d7683b
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Tue May 16 15:44:16 2023 +0300
    
        Merge branch 'master' into fix/AG-19184
    
    commit 842988208278d6121a1c0d96c4f68e0ff0678ca3
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Mon May 15 20:19:14 2023 +0300
    
        improve tests, get rid of trimmedInput variable
    
    commit c4b1dcf274d95ffda89550e5ff4e899a89e08415
    Author: Stanislav A <s.atroschenko@adguard.com>
    Date:   Fri May 12 20:28:12 2023 +0300
    
        unescape quotes for callback matching argument

--- src/helpers/string-utils.js
+++ src/helpers/string-utils.js
@@ -48,7 +48,14 @@ export const toRegExp = (input = '') => {
     if (input[0] === FORWARD_SLASH && input[input.length - 1] === FORWARD_SLASH) {
         return new RegExp(input.slice(1, -1));
     }
-    const escaped = input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+    const escaped = input
+        // remove quotes' escapes for cases where scriptlet rule argument has own escaped quotes
+        // e.g #%#//scriptlet('prevent-setTimeout', '.css(\'display\',\'block\');')
+        .replace(/\\'/g, '\'')
+        .replace(/\\"/g, '"')
+        // escape special characters for following RegExp construction
+        .replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     return new RegExp(escaped);
 };
 
